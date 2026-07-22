0eae2e9eb729e8a00633406e036b8e099c559855
nick fitzsimons <nick_fitzsimons@hotmail.com>
2026-07-22 10:40:04 +0100

    initial Commit - Time API

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstCloudApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TimeController : ControllerBase
+{
+    // GET /time
+    [HttpGet]
+    public IActionResult GetCurrentTime()
+    {
+        return Ok(new
+        {
+            Service = "Cloud Time Service",
+            CurrentUtcTime = DateTime.UtcNow,
+            Message = "Hello from Azure!"
+        });
+    }
+}
